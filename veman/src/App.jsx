2af8ae1da1896a38a8fe2caf8026fb00d8ce8400
Mukhil <mukhil.cs21@bitsathy.ac.in>
2024-06-26 22:37:31 +0530

    BusMaster Frontend

--- veman/src/App.jsx
+++ veman/src/App.jsx
@@ -2,6 +2,8 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home2 from './pages/Home2'
+import DriverMaster from './pages/DriverMaster'
+import BusMaster from './pages/BusMaster'
 
 function App() {
  
@@ -14,7 +16,8 @@ function App() {
         <Route index element={<Login/>}/>
         <Route path='/Home' element={<Home/>} />
         <Route path='/Home2' element={<Home2/>}/>
-
+        <Route path='/DriverMaster' element={<DriverMaster/>}/>
+        <Route path='/BusMaster' element={<BusMaster/>}/>
       </Routes>
       </BrowserRouter>
     </>
